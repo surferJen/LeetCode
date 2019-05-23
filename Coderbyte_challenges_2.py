786d81ddb0445c72ab4553bc92084b391e3719fe
Jae Kim <jy.kim8295@gmail.com>
2019-05-23 15:39:49 -0700

    Coderyte_challenges_2.py start LetterChanges(str) problem

---
+++ Coderbyte_challenges_2.py
@@ -0,0 +1,17 @@
+# CHALLENGE 1
+
+# Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
+
+# Sample Test Cases
+
+# Input:"hello*3"
+
+# Output:Ifmmp*3
+
+
+# Input:"fun times!"
+
+# Output:gvO Ujnft!
+
+def LetterChanges(str):
+    
