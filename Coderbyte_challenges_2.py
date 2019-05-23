a24ecba7b1d8942b713cfa502d2a2214b79efe6c
Jae Kim <jy.kim8295@gmail.com>
2019-05-23 16:07:32 -0700

    Coderbyte_challenges_2.py LetterChanges(str) CHALLENGE 1, COMPLETE

--- Coderbyte_challenges_2.py
+++ Coderbyte_challenges_2.py
@@ -15,3 +15,26 @@
 
 def LetterChanges(str):
     
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    vowels = ["a", "e", "i", "o", "u"]
+
+    answer_lst = []
+
+    for char in str:
+        if char in alphabet: 
+            answer_lst.append(alphabet[alphabet.index(char) + 1])
+        else:
+            answer_lst.append(char)
+    
+    for item in answer_lst:
+        if item in vowels:
+            answer_lst[answer_lst.index(item)] = item.upper()
+
+    answer = "".join(answer_lst)
+    return answer
+
+print(LetterChanges("fun times!"))
+print(LetterChanges("hello*3"))
+
+#CHALLENGE 2
