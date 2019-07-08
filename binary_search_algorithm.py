de6837f07dd26d02ffd9b057a101b4064f63fee0
Jennifer Kim <jenniferkim@Jennifers-MacBook-Pro.local>
2019-07-07 21:35:52 -0700

    binary_search_algorithm.py COMPLETED

--- binary_search_algorithm.py
+++ binary_search_algorithm.py
@@ -1,1 +1,29 @@
 #INTERVIEW CAKE
+
+def binary_search(target, nums):
+    """See if target appears in nums"""
+
+    floor_index = -1
+    ceiling_index = len(nums)
+
+    while floor_index + 1 < ceiling_index:
+
+        distance = ceiling_index - floor_index
+        half_distance = distance // 2
+        guess_index = floor_index + half_distance
+
+        guess_value = nums[guess_index]
+
+        if guess_value == target:
+            return True
+        
+        if guess_value > target: 
+            ceiling_index = guess_index
+        
+        else:
+            floor_index = guess_index
+        
+        return False 
+
+binary_search(3, [4, 5, 6, 2, 3, 6, 9, 0])
+binary_search(3, [4, 5, 8, 2, 9, 9, 0, 2])
