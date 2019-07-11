aabbbe57fd860c889db2565e59ed6d2d61c0a338
Jennifer Kim <jenniferkim@Jennifers-MacBook-Pro.local>
2019-07-10 17:48:23 -0700

    linked_list_kth_to_last_node.py COMPLETED

--- linked_list_kth_to_last_node.py
+++ linked_list_kth_to_last_node.py
@@ -1,3 +1,62 @@
 #You have a linked list and want to find the kth to last node.
 
 #Write a function kth_to_last_node() that takes an integer k and the head_node of a singly-linked list, and returns the kth to last node in the list.
+
+class LinkedListNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+a = LinkedListNode("Angel Food")
+b = LinkedListNode("Bundt")
+c = LinkedListNode("Cheese")
+d = LinkedListNode("Devil's Food")
+e = LinkedListNode("Eccles")
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e 
+
+#Returns the node with value "Devil's Food" (the 2nd to last node)
+
+def kth_to_last_node(k, head_node):
+    
+    if k < 1:
+        raise ValueError('Invalid input')
+
+    current_node = head_node
+
+    counter = 0
+
+    
+    while current_node is not None:
+        counter += 1
+        
+        if current_node.next is not None:
+            current_node = current_node.next
+    
+    current_node = head_node
+    index_nth_node = counter - k
+    nth_node = ""
+
+    while index_nth_node >= 0:
+        index_nth_node -= 1
+        current_node = current_node.next
+    
+    return nth_node
+
+
+print(kth_to_last_node(2, a))
+
+    
+
+    
+
+    
+
+
+
+    
+
